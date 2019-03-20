117c9b264f0655388e0c2e6e766795df30e82a7b
bof <boris.graeff@gmail.com>
2019-03-20 12:22:58 +0100

    Set up toggle menu behaviour on mobile

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,20 +1,25 @@
 <template>
   <header>
-    <div>
-      <h1>
-        {{ $t('title') }}
-      </h1>
+    <h1>
+      {{ $t('title') }}
+    </h1>
 
-      <a :href="$t('externals.booking', { lang: $i18n.locale.toUpperCase() })">
-        {{ $t('common.actions.booking') }}
-      </a>
+    <label class="menu-button" for="show-menu-button">|||</label>
+    <input type="checkbox" id="show-menu-button" />
 
-      <nav class="Header__Menu">
-        <NuxtLink v-for="page in $options.pages"
-                  :key="page"
-                  :to="$i18n.path($t(`routes.${page}.url`))" exact>
-          {{ $t(`routes.${page}.name`) }}
-        </NuxtLink>
+    <nav class="nav">
+      <div>
+        <a :href="$t('externals.booking', { lang: $i18n.locale.toUpperCase() })">
+          {{ $t('common.actions.booking') }}
+        </a>
+
+        <ul>
+          <li v-for="page in $options.pages" :key="page">
+            <NuxtLink :to="$i18n.path($t(`routes.${page}.url`))" exact>
+              {{ $t(`routes.${page}.name`) }}
+            </NuxtLink>
+          </li>
+        </ul>
 
         <NuxtLink v-if="$i18n.locale === 'fr'" to="/en" exact>
           {{ $t('routes.lang.en') }}
@@ -22,8 +27,8 @@
         <NuxtLink v-if="$i18n.locale === 'en'" to="/" exact>
           {{ $t('routes.lang.fr') }}
         </NuxtLink>
-      </nav>
-    </div>
+      </div>
+    </nav>
   </header>
 </template>
 
@@ -32,3 +37,28 @@ export default {
   pages: ['home', 'rooms', 'plan', 'tearoom', 'restaurant', 'seminars', 'village']
 }
 </script>
+
+<style scoped lang="scss">
+
+  .menu-button {
+    display: none;
+
+    + input {
+      display: none;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    .nav {
+      display: none;
+    }
+
+    .menu-button + input:checked + .nav {
+      display: block;
+    }
+
+    .menu-button {
+      display: inline-block;
+    }
+  }
+</style>
