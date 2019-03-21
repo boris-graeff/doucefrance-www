ab1b580362331c77be8d172bc7cb032fb0c0ee33
bof <boris.graeff@gmail.com>
2019-03-21 12:14:45 +0100

    Fill tearoom page with content

--- pages/_lang/tearoom.vue
+++ pages/_lang/tearoom.vue
@@ -1,5 +1,35 @@
 <template>
   <section>
-    <h1>Tea Room</h1>
+    <img src="/images/tearoom/tea-time-douce-france.jpg" class="page-header"/>
+
+    <div class="page-content">
+      <div class="blocks">
+        <div>
+          <h2>Tea time</h2>
+
+          <p>Under the cloister, in the courtyard garden or simply in one of our interior lounges, taste our home made pastries, our crepes, our generous ice creams, and appreciate our large choice of teas from around the world, our traditional hot chocolate or our fresh fruit juices.</p>
+        </div>
+
+        <div>
+
+          <h2>The cloister and the courtyard garden</h2>
+
+          <p>Bordering the little river Veules, the scent of roses and honeysuckle, and the tinkling of the fountains with help you to rediscover a feeling of calm and tranquility</p>
+        </div>
+
+        <div>
+          <h2>Salon intérieur</h2>
+          <p>Petits déjeuners, restauration légère, salon de thé, terrasse au bord de l'eau. Farniente, quiétude et douceur de vivre.</p>
+        </div>
+
+      </div>
+    </div>
+
+    <img src="/images/tearoom/lounge-interior-douce-france.jpg" class="page-footer"/>
   </section>
 </template>
+
+<style scoped lang="scss">
+  @import '~@/style/vars';
+
+</style>
